dcc00804dce2f63235a58c2f7427e29d47f238d0
Joe Heck <heckj@mac.com>
2024-12-01 18:39:36 -0800

    add platform stanza, and depdendency on swift argument parser

--- Package.swift
+++ Package.swift
@@ -5,20 +5,29 @@ import PackageDescription
 
 let package = Package(
     name: "formic",
+    platforms: [.macOS(.v13), .iOS(.v16)],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "formic",
             targets: ["formic"]),
     ],
+    dependencies: [
+        .package(
+              url: "https://github.com/apple/swift-argument-parser.git",
+              .upToNextMajor(from: "1.5.0")),
+    ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "formic"),
+            name: "formic",
+            dependencies: [.product(name: "ArgumentParser", package: "swift-argument-parser")]
+        ),
         .testTarget(
             name: "formicTests",
             dependencies: ["formic"]
         ),
-    ]
+    ],
+    swiftLanguageModes: [.v6]
 )
