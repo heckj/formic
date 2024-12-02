c4c03dbfced31188122cf204d8f6f6d22c20810a
Joe Heck <heckj@mac.com>
2024-12-02 12:26:42 -0800

    finish capitalizing module name

--- Package.swift
+++ Package.swift
@@ -9,7 +9,7 @@ let package = Package(
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "Formic",
-            targets: ["formic"]),
+            targets: ["Formic"]),
     ],
     dependencies: [
         .package(
@@ -23,12 +23,12 @@ let package = Package(
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "formic",
+            name: "Formic",
             dependencies: [.product(name: "ArgumentParser", package: "swift-argument-parser")]
         ),
         .testTarget(
-            name: "formicTests",
-            dependencies: ["formic"]
+            name: "FormicTests",
+            dependencies: ["Formic"]
         ),
     ],
     swiftLanguageModes: [.v6]
