b0017f785cfdc3ebd7c4382260fbf0ba58ec105b
Joe Heck <heckj@mac.com>
2024-12-02 16:00:47 -0800

    let's not destroy our ability to read STDOUT, shall we?

--- Sources/formic/Pipe+string.swift
+++ Sources/formic/Pipe+string.swift
@@ -1,19 +1,13 @@
-//#if canImport(FoundationEssentials)
-//import FoundationEssentials
-//#else
 import Foundation
 
-//#endif
-
 extension Pipe {
-    func string() -> String? {
-        let data = self.fileHandleForReading.readDataToEndOfFile()
-        let result: String?
-        if let string = String(data: data, encoding: String.Encoding.utf8) {
-            result = string
-        } else {
-            result = nil
+    public func string() throws -> String? {
+        guard let data = try self.fileHandleForReading.readToEnd() else {
+            return nil
         }
-        return result
+        guard let stringValue = String(data: data, encoding: String.Encoding.utf8) else {
+            return nil
+        }
+        return stringValue
     }
 }
