70211b44c784a6043d0100092377ade80661580f
Joe Heck <heckj@mac.com>
2024-12-02 16:06:53 -0800

    adding dependency to SwiftCommand

--- Package.swift
+++ Package.swift
@@ -6,25 +6,28 @@ let package = Package(
     name: "Formic",
     platforms: [.macOS(.v13), .iOS(.v16)],
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "Formic",
             targets: ["Formic"]),
     ],
     dependencies: [
         .package(
-              url: "https://github.com/apple/swift-argument-parser.git",
-              .upToNextMajor(from: "1.5.0")),
+            url: "https://github.com/apple/swift-argument-parser.git",
+            .upToNextMajor(from: "1.5.0")),
         .package(
-              url: "https://github.com/swiftlang/swift-format.git",
-              .upToNextMajor(from: "600.0.0"))
+            url: "https://github.com/Zollerboy1/SwiftCommand.git",
+            from: "1.4.0"),
+        .package(
+            url: "https://github.com/swiftlang/swift-format.git",
+            .upToNextMajor(from: "600.0.0"))
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
         .target(
             name: "Formic",
-            dependencies: [.product(name: "ArgumentParser", package: "swift-argument-parser")]
+            dependencies: [
+                .product(name: "ArgumentParser", package: "swift-argument-parser"),
+                .product(name: "SwiftCommand", package: "SwiftCommand")
+            ]
         ),
         .testTarget(
             name: "FormicTests",
