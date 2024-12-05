54509c74fc64c00e0ff283a648eef451ce91970c
Joe Heck <heckj@mac.com>
2024-12-04 16:13:49 -0800

    SSHCreds

--- Sources/Formic/Host.swift
+++ Sources/Formic/Host.swift
@@ -1,20 +1,5 @@
 import Foundation
 
-/// SSH Credentials for accessing a remote host
-struct SSHAccessCredentials {
-    let username: String
-    let identityFile: String
-    
-    init(username: String, identityFile: String) {
-        self.username = username
-        self.identityFile = identityFile
-    }
-    
-    // TODO:
-    // default identity file using FileManager - choose from what's available
-    // default username from $USER in processInfo
-}
-
 struct Host {
     let os: OperatingSystem
     let remote: Bool
