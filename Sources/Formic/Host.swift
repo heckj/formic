5e3151613190765eb603da6cb27c67c628d0a8aa
Joe Heck <heckj@mac.com>
2024-12-06 09:31:22 -0800

    adding remote vs local specifics on name

--- Sources/Formic/Host.swift
+++ Sources/Formic/Host.swift
@@ -27,12 +27,39 @@ public struct Host: Sendable {
     ///   - sshPort: the ssh port, defaults to `22`.
     ///   - sshUser: the ssh user, defaults to the username of the current user.
     ///   - sshIdentityFile: The ssh identity file, defaults to standard key locations for ssh.
+    ///
+    ///   The initializer may return nil if the name isn't a valid network address.
+    ///
+    ///   Use the name `localhost` to ensure all commands are run locally.
+    ///   Use the name `127.0.0.1` to access a remote host through a port forwarding setup.
     public init?(_ name: String, sshPort: Int = 22, sshUser: String? = nil, sshIdentityFile: String? = nil) throws {
         let creds = try SSHAccessCredentials(username: sshUser, identityFile: sshIdentityFile)
         if let address = NetworkAddress(name) {
-            self.init(remote: false, address: address, sshPort: sshPort, sshAccessCredentials: creds)
+            if name == "localhost" {
+                self.init(remote: false, address: address, sshPort: sshPort, sshAccessCredentials: creds)
+            } else {
+                self.init(remote: true, address: address, sshPort: sshPort, sshAccessCredentials: creds)
+            }
         } else {
             return nil
+        }
+    }
+
+    /// Creates a new host using the NetworkAddress you provide.
+    /// - Parameters:
+    ///   - networkAddresss: The network address of the host.
+    ///   - sshPort: the ssh port, defaults to `22`.
+    ///   - sshUser: the ssh user, defaults to the username of the current user.
+    ///   - sshIdentityFile: The ssh identity file, defaults to standard key locations for ssh.
+    ///
+    ///   Use the name `localhost` to ensure all commands are run locally.
+    ///   Use the name `127.0.0.1` to access a remote host through a port forwarding setup.
+    public init(_ networkAddresss: NetworkAddress, sshPort: Int = 22, sshUser: String? = nil, sshIdentityFile: String? = nil) throws {
+        let creds = try SSHAccessCredentials(username: sshUser, identityFile: sshIdentityFile)
+        if networkAddresss.dnsName == "localhost" {
+            self.init(remote: false, address: networkAddresss, sshPort: sshPort, sshAccessCredentials: creds)
+        } else {
+            self.init(remote: true, address: networkAddresss, sshPort: sshPort, sshAccessCredentials: creds)
         }
     }
 
@@ -42,12 +69,17 @@ public struct Host: Sendable {
     ///   - sshPort: the ssh port, defaults to `22`.
     ///   - sshUser: the ssh user, defaults to the username of the current user.
     ///   - sshIdentityFile: The ssh identity file, defaults to standard key locations for ssh.
+    ///
+    ///   Use the name `localhost` to ensure all commands are run locally.
+    ///   Use the name `127.0.0.1` to access a remote host through a port forwarding setup.
     public static func resolve(
         _ name: String, sshPort: Int = 22, sshUser: String? = nil, sshIdentityFile: String? = nil
     ) async throws -> Host {
         let creds = try SSHAccessCredentials(username: sshUser, identityFile: sshIdentityFile)
-        if let address = await NetworkAddress.resolve(name) {
-            return Host(remote: false, address: address, sshPort: sshPort, sshAccessCredentials: creds)
+        if name == "localhost" {
+            return Host(remote: false, address: .localhost, sshPort: sshPort, sshAccessCredentials: creds)
+        } else if let address = await NetworkAddress.resolve(name) {
+            return Host(remote: true, address: address, sshPort: sshPort, sshAccessCredentials: creds)
         } else {
             throw CommandError.failedToResolveHost(name: name)
         }
