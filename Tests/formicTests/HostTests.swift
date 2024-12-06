91c03e27b2d6bfaf78b6a7b0c241c5195e43273b
Joe Heck <heckj@mac.com>
2024-12-06 09:42:08 -0800

    enshrine naming convention for remote vs local in tests

--- Tests/formicTests/HostTests.swift
+++ Tests/formicTests/HostTests.swift
@@ -4,15 +4,16 @@ import Testing
 
 @testable import Formic
 
+struct TestFileSystemAccess: LocalSystemAccess {
+    func fileExists(atPath: String) -> Bool {
+        atPath.contains("id_dsa")
+    }
+    let homeDirectory: URL = URL(filePath: "/home/docker-user")
+    let username: String? = "docker-user"
+}
+
 @Test("Host initializer")
 func initHost() async throws {
-    struct TestFileSystemAccess: LocalSystemAccess {
-        func fileExists(atPath: String) -> Bool {
-            atPath.contains("id_dsa")
-        }
-        let homeDirectory: URL = URL(filePath: "/home/docker-user")
-        let username: String? = "docker-user"
-    }
 
     let host = try withDependencies { dependencyValues in
         dependencyValues.localSystemAccess = TestFileSystemAccess()
@@ -27,4 +28,26 @@ func initHost() async throws {
     #expect(host?.sshAccessCredentials != nil)
     #expect(host?.sshAccessCredentials.username == "docker-user")
     #expect(host?.sshAccessCredentials.identityFile == "/home/docker-user/.ssh/id_dsa")
+}
+
+@Test("Host initializer using localhost name should be marked local")
+func initHostWithLocalHostName() async throws {
+    let host = try withDependencies { dependencyValues in
+        dependencyValues.localSystemAccess = TestFileSystemAccess()
+    } operation: {
+        try Host("localhost")
+    }
+    #expect(host != nil)
+    #expect(host?.remote == false)
+}
+
+@Test("Host initializer using localhost address should be marked remote")
+func initHostWithLocalHostAddress() async throws {
+    let host = try withDependencies { dependencyValues in
+        dependencyValues.localSystemAccess = TestFileSystemAccess()
+    } operation: {
+        try Host("127.0.0.1")
+    }
+    #expect(host != nil)
+    #expect(host?.remote == true)
 }
