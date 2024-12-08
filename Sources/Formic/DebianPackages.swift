ebb403bf585eb5dc0aeeb2caf1f69882e3dda05c
Joe Heck <heckj@mac.com>
2024-12-08 14:09:15 -0800

    getting there, but quirky with the newlines - doesn't handle an abrupt stop well

--- Sources/Formic/DebianPackages.swift
+++ Sources/Formic/DebianPackages.swift
@@ -67,9 +67,134 @@ public struct DebianPackage: QueryableResource {
     public static func parse(_ output: String) throws -> DebianPackage {
         fatalError("not implemented")
     }
-
 }
 
 // there's stuff I want to just "ask about" and report on, and stuff I want to "change"
 // QueryableResource vs. DeclaredResource
 // for both state and information bits that I query - I want to track "when I last asked" - when it was last updated.
+
+struct DpkgState: Sendable, Hashable {
+    enum DesiredState: String, Sendable, Hashable {
+        case unknown = "u"
+        case install = "i"
+        case remove = "r"
+        case purge = "p"
+        case hold = "h"
+    }
+
+    enum StatusCode: String, Sendable, Hashable {
+        case notInstalled = "n"
+        case installed = "i"
+        case configFiles = "c"
+        case unpacked = "u"
+        case failedConfig = "f"
+        case halfInstalled = "h"
+        case triggerAwait = "w"
+        case triggerPending = "t"
+    }
+
+    enum ErrCode: String, Sendable, Hashable {
+        case reinstall = "r"
+        case none = " "
+    }
+
+    let desiredState: DesiredState
+    let statusCode: StatusCode
+    let errCode: ErrCode
+
+    let name: String
+    let version: String
+    let architecture: String
+    let description: String
+}
+
+// https://swiftpackageindex.com/pointfreeco/swift-parsing#user-content-getting-started
+// https://pointfreeco.github.io/swift-parsing/0.10.0/documentation/parsing/
+
+extension DpkgState {
+    // parsing individual lines
+    struct PackageCodes: Parser {
+        var body: some Parser<Substring, (DesiredState, StatusCode, ErrCode)> {
+            OneOf {
+                DesiredState.unknown.rawValue.map { DesiredState.unknown }
+                DesiredState.install.rawValue.map { DesiredState.install }
+                DesiredState.remove.rawValue.map { DesiredState.remove }
+                DesiredState.purge.rawValue.map { DesiredState.purge }
+                DesiredState.hold.rawValue.map { DesiredState.hold }
+            }
+            OneOf {
+                StatusCode.notInstalled.rawValue.map { StatusCode.notInstalled }
+                StatusCode.installed.rawValue.map { StatusCode.installed }
+                StatusCode.configFiles.rawValue.map { StatusCode.configFiles }
+                StatusCode.unpacked.rawValue.map { StatusCode.unpacked }
+                StatusCode.halfInstalled.rawValue.map { StatusCode.halfInstalled }
+                StatusCode.triggerAwait.rawValue.map { StatusCode.triggerAwait }
+                StatusCode.triggerPending.rawValue.map { StatusCode.triggerPending }
+            }
+            OneOf {
+                ErrCode.reinstall.rawValue.map { ErrCode.reinstall }
+                ErrCode.none.rawValue.map { ErrCode.none }
+            }
+        }
+    }
+
+    struct PackageStatus: Parser {
+        var body: some Parser<Substring, DpkgState> {
+            Parse(DpkgState.init) {
+                PackageCodes()
+                Skip {
+                    // whitespace
+                    Whitespace()
+                    //Prefix { $0.isWhitespace }
+                }
+                // package name
+                Prefix { !$0.isWhitespace }.map(String.init)
+                Skip {
+                    Whitespace()
+                    // whitespace
+                    //Prefix { $0.isWhitespace }
+                }
+                // version
+                Prefix { !$0.isWhitespace }.map(String.init)
+                Skip {
+                    Whitespace()
+                    // whitespace
+                    //Prefix { $0.isWhitespace }
+                }
+                // architecture
+                Prefix { !$0.isWhitespace }.map(String.init)
+                Skip {
+                    // whitespace
+                    Whitespace()
+                    //Prefix { $0.isWhitespace }
+                }
+                // description
+                PrefixUpTo("\n").map(String.init)
+                "\n"
+            }
+        }
+    }
+}
+
+extension DpkgState {
+    // parsing `dpkg -l` output
+    struct DpkgHeader: Parser {
+        var body: some Parser<Substring, Void> {
+            Skip {
+                PrefixThrough("========")
+            }
+            Skip {
+                PrefixThrough("\n")
+            }
+        }
+    }
+
+    struct PackageList: Parser {
+        var body: some Parser<Substring, [DpkgState]> {
+            DpkgState.DpkgHeader()
+            Many(1...) {
+                PackageStatus()
+            }
+        }
+    }
+}
