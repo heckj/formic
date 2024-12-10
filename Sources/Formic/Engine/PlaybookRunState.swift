af3eb25b9fc6903ad911cd3609c8311b48775545
Joe Heck <heckj@mac.com>
2024-12-10 11:49:05 -0800

    engine mode fleshed out, and scheduling setup

--- Sources/Formic/Engine/PlaybookRunState.swift
+++ Sources/Formic/Engine/PlaybookRunState.swift
@@ -1,7 +1,7 @@
 /// The state of execution for a playbook.
 public enum PlaybookRunState: Sendable, Hashable, Codable {
     /// The playbook is scheduled to run, but hasn't yet started.
-    case pending
+    case scheduled
     /// The playbook is in progress.
     case running
     /// The playbook is finished.
