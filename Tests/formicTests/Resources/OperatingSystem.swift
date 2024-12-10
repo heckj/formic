e77e7af4dae8ec3e893613fe74e03b1239bb0615
Joe Heck <heckj@mac.com>
2024-12-09 16:40:20 -0800

    fleshing out the resource structures based on what I've created so far

--- Tests/formicTests/Resources/OperatingSystem.swift
+++ Tests/formicTests/Resources/OperatingSystem.swift
@@ -4,13 +4,31 @@ import Testing
 
 @testable import Formic
 
+@Test("parsing uname output for determining type of operating system")
+func testOSKindParsing() async throws {
+    let parser = OperatingSystem.UnameParser()
+    #expect(try parser.parse("Linux\n") == .linux)
+    #expect(try parser.parse("Darwin\n") == .macOS)
+    #expect(try parser.parse("Linux") == .linux)
+    #expect(try parser.parse("Darwin") == .macOS)
+}
+
+@Test("verify string based initializer for OperatingSystem")
+func testOSStringInitializer() async throws {
+    #expect(OperatingSystem("Darwin").name == .macOS)
+    #expect(OperatingSystem("macOS").name == .macOS)
+    #expect(OperatingSystem("linux").name == .linux)
+    #expect(OperatingSystem("Linux").name == .linux)
+    #expect(OperatingSystem("FreeBSD").name == .unknown)
+}
+
 @Test("test resource building blocks")
 func testResourceBuildingBlocks() async throws {
 
     let shellResult = try withDependencies {
         $0.commandInvoker = TestCommandInvoker(command: ["uname"], presentOutput: "Linux\n")
     } operation: {
-        try OperatingSystem.shellcommand.run(host: .localhost)
+        try OperatingSystem.singularInquiry.run(host: .localhost)
     }
 
     // results proxied for a linux host
@@ -19,19 +37,34 @@ func testResourceBuildingBlocks() async throws {
     #expect(shellResult.stderrString == nil)
 
     let stdout = try #require(shellResult.stdoutString)
-    let parsedOS = try OperatingSystem.parse(stdout)
-    #expect(parsedOS == .linux)
+    let parsedOS = OperatingSystem.parse(stdout)
+    #expect(parsedOS.name == .linux)
 }
 
-@Test("test queried operating system")
+@Test("test singular resource query for operating system")
 func testOperatingSystemQuery() async throws {
 
     let (parsedOS, _) = try withDependencies {
         $0.commandInvoker = TestCommandInvoker(command: ["uname"], presentOutput: "Linux\n")
         $0.date.now = Date(timeIntervalSince1970: 1_234_567_890)
     } operation: {
-        try OperatingSystem.queryState(from: .localhost)
+        try OperatingSystem.findResource(from: .localhost)
     }
 
-    #expect(parsedOS == .linux)
+    #expect(parsedOS.name == .linux)
+}
+
+@Test("test instance resource query for operating system")
+func testOperatingSystemInstanceQuery() async throws {
+
+    let instance = OperatingSystem(.macOS)
+
+    let (parsedOS, _) = try withDependencies {
+        $0.commandInvoker = TestCommandInvoker(command: ["uname"], presentOutput: "Linux\n")
+        $0.date.now = Date(timeIntervalSince1970: 1_234_567_890)
+    } operation: {
+        try instance.queryResource(from: .localhost)
+    }
+
+    #expect(parsedOS.name == .linux)
 }
