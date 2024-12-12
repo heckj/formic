4b03baf822695d75016fb821313bd2bb6ce7fecd
Joe Heck <heckj@mac.com>
2024-12-12 15:04:49 -0800

    remove sync run from Playbook declaration

--- Sources/Formic/Playbook.swift
+++ Sources/Formic/Playbook.swift
@@ -41,35 +41,6 @@ public struct Playbook: Identifiable, Sendable {
             }
         }
     }
-
-    /// Runs the playlist synchronously for each host in order, printing out the results.
-    public func runSync() throws {
-        for host in hosts {
-            for command in commands {
-                let result = try command.run(host: host)
-                // TODO: add check to see if Command declaration says to ignore the failure.
-                if result.returnCode != 0 {
-                    // failure
-                    print("❌ \(command) (rc=\(result.returnCode))")
-                    if let stdout = result.stdoutString {
-                        print("  out: \(stdout)")
-                    }
-                    if let stderr = result.stderrString {
-                        print("  out: \(stderr)")
-                    }
-                } else {
-                    // success
-                    print("✅ \(command)")
-                    if let stdout = result.stdoutString {
-                        print("  out: \(stdout)")
-                    }
-                    if let stderr = result.stderrString {
-                        print("  out: \(stderr)")
-                    }
-                }
-            }
-        }
-    }
 }
 
 extension Playbook: Hashable {}
