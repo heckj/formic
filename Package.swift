574282037e8f613e977fe7330161fe8bbdc1b721
Joe Heck <heckj@mac.com>
2024-12-17 14:17:34 -0800

    test branch experiment for validating swift-testing

--- Package.swift
+++ Package.swift
@@ -19,11 +19,12 @@ let package = Package(
             url: "https://github.com/apple/swift-async-dns-resolver",
             .upToNextMajor(from: "0.1.0")
         ),
-        // .package(url: "https://github.com/Zollerboy1/SwiftCommand.git", from: "1.4.0"),
         .package(url: "https://github.com/pointfreeco/swift-parsing", from: "0.13.0"),
         .package(url: "https://github.com/pointfreeco/swift-dependencies", from: "1.0.0"),
         .package(url: "https://github.com/swiftlang/swift-format.git",
-            .upToNextMajor(from: "600.0.0"))
+            .upToNextMajor(from: "600.0.0")),
+        // .package(url: "https://github.com/Zollerboy1/SwiftCommand.git", from: "1.4.0"),
+        .package(url: "https://github.com/swiftlang/swift-testing/", branch: "jgrynspan/162-redesign-value-capture"),
     ],
     targets: [
         .target(
@@ -31,15 +32,16 @@ let package = Package(
             dependencies: [
                 .product(name: "ArgumentParser", package: "swift-argument-parser"),
                 .product(name: "AsyncDNSResolver", package: "swift-async-dns-resolver"),
-                // .product(name: "SwiftCommand", package: "SwiftCommand"),
                 .product(name: "Parsing", package: "swift-parsing"),
                 .product(name: "Dependencies", package: "swift-dependencies"),
+                // .product(name: "SwiftCommand", package: "SwiftCommand"),
             ]
         ),
         .testTarget(
             name: "FormicTests",
             dependencies: [
-                "Formic"
+                "Formic",
+                .product(name: "Testing", package: "swift-testing")
             ]
         ),
     ],
