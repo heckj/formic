78754731daacdd91cddb31caa6b90ac076c4970c
Joe Heck <heckj@mac.com>
2024-12-20 13:24:29 -0800

    expand testing on backoff

--- Tests/formicTests/BackoffTests.swift
+++ Tests/formicTests/BackoffTests.swift
@@ -10,6 +10,23 @@ func testBackoffDelayLogicNone() async throws {
     #expect(strategy.delay(for: 10) == .seconds(0))
 }
 
+@Test("verify backoff builtins")
+func testBackoffBuiltings() async throws {
+    let backoff = Backoff.never
+    #expect(backoff.maxRetries == 0)
+    #expect(backoff.strategy == .none)
+
+    let backoff2 = Backoff.default
+    #expect(backoff2.maxRetries == 3)
+    #expect(backoff2.strategy == .fibonacci(maxDelay: .seconds(10)))
+}
+
+@Test("verify backoff initializer with negative value")
+func testBackoffDelayInitnegative() async throws {
+    let negBackoff = Backoff(maxRetries: -1, strategy: .none)
+    #expect(negBackoff.maxRetries == 0)
+}
+
 @Test("verify backoff logic - .constant")
 func testBackoffDelayLogicConstant() async throws {
     let strategy = Backoff.Strategy.constant(delay: .seconds(3.5))
