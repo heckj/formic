4f3acf6b6e135de26e15e632531defc9ac491a77
Joe Heck <heckj@mac.com>
2024-12-31 16:42:32 -0800

    fixing and testing verifyAccess logic

---
+++ Tests/formicTests/Commands/VerifyAccessCommandTests.swift
@@ -0,0 +1,47 @@
+import Dependencies
+import Foundation
+import Testing
+
+@testable import Formic
+
+@Test("test invoking a verify access command with failure response")
+func testInvokingVerifyAccessFail() async throws {
+
+    let testInvoker = TestCommandInvoker()
+
+    let host = try await withDependencies { dependencyValues in
+        dependencyValues.localSystemAccess = TestFileSystemAccess(
+            dnsName: "somewhere.com", ipAddressesToUse: ["8.8.8.8"])
+    } operation: {
+        try await Host.resolve("somewhere.com")
+    }
+
+    let cmdOut = try await withDependencies {
+        $0.commandInvoker = testInvoker
+    } operation: {
+        try await VerifyAccess().run(host: host)
+    }
+    #expect(cmdOut.stderrString == "Unable to verify access.")
+    #expect(cmdOut.returnCode == -1)
+}
+
+@Test("test invoking a verify access command success")
+func testInvokingVerifyAccessSuccess() async throws {
+
+    let testInvoker = TestCommandInvoker()
+
+    let host = try await withDependencies { dependencyValues in
+        dependencyValues.localSystemAccess = TestFileSystemAccess(
+            dnsName: "somewhere.com", ipAddressesToUse: ["8.8.8.8"])
+    } operation: {
+        try await Host.resolve("somewhere.com")
+    }
+
+    let cmdOut = try await withDependencies {
+        $0.commandInvoker = testInvoker.addSuccess(command: "echo 'hello'", presentOutput: "hello\n")
+    } operation: {
+        try await VerifyAccess().run(host: host)
+    }
+    #expect(cmdOut.stdoutString == "hello")
+    #expect(cmdOut.returnCode == 0)
+}
