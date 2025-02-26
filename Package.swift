ff3048d32ae44f748da991fdfca1c86bec3bcaed
Joe Heck <heckj@mac.com>
2025-02-26 11:31:00 -0800

    clean up package.swift formatting with swift-format

--- Package.swift
+++ Package.swift
@@ -8,12 +8,14 @@ let package = Package(
     products: [
         .library(
             name: "Formic",
-            targets: ["Formic"]),
+            targets: ["Formic"]
+        )
     ],
     dependencies: [
         .package(
             url: "https://github.com/apple/swift-argument-parser.git",
-            .upToNextMajor(from: "1.5.0")),
+            .upToNextMajor(from: "1.5.0")
+        ),
         .package(
             url: "https://github.com/apple/swift-async-dns-resolver",
             .upToNextMajor(from: "0.1.0")
@@ -21,7 +23,8 @@ let package = Package(
         .package(url: "https://github.com/apple/swift-log.git", from: "1.0.0"),
         .package(url: "https://github.com/pointfreeco/swift-parsing", from: "0.13.0"),
         .package(url: "https://github.com/pointfreeco/swift-dependencies", from: "1.0.0"),
-        .package(url: "https://github.com/swiftlang/swift-format.git",
+        .package(
+            url: "https://github.com/swiftlang/swift-format.git",
             .upToNextMajor(from: "600.0.0")),
         .package(url: "https://github.com/neallester/swift-log-testing.git", from: "0.0.1"),
     ],
@@ -40,11 +43,11 @@ let package = Package(
             name: "FormicTests",
             dependencies: [
                 "Formic",
-                .product(name: "SwiftLogTesting", package: "swift-log-testing")
+                .product(name: "SwiftLogTesting", package: "swift-log-testing"),
             ],
             resources: [
                 .copy("formicTests/Fixtures/id_ed25519"),
-                .copy("formicTests/Fixtures/id_ed25519.pub")
+                .copy("formicTests/Fixtures/id_ed25519.pub"),
             ]
         ),
     ],
